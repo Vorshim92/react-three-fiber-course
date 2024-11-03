2e37ea4c1b813faf88505d117e9ed1472d6c93ae
Stefano Scalfari <vorshim92@gmail.com>
2024-11-03 23:38:45 +0100

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 function App() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
-      <OrbitControls />
-      <mesh position-x={-0.6}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshToonMaterial color="green" side={THREE.FrontSide} />
-      </mesh>
-      <mesh position-x={0.6} position-z={-1}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshToonMaterial color="green" side={THREE.BackSide} />
-      </mesh>
+    <Canvas camera={{ position: [0, 3, 8] }}>
+      {/* Lighting */}
       <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 0, 3]} intensity={1} />
-      <directionalLight position={[1, 3, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 5]} intensity={0.5} />
+
+      {/* Objects */}
+      <group position={[-2, -2, 0]} scale={2} rotation-y={Math.PI / 4}>
+        <mesh position-x={-1}>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <mesh position-x={1}>
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+      </group>
     </Canvas>
   );
 }
