f7139e8586e59cbf6dce02a1113e777253b6ce3a
Stefano Scalfari <vorshim92@gmail.com>
2024-11-04 16:19:27 +0100

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, SpotLight, useHelper } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+
+import { useControls } from "leva";
+
+import * as THREE from "three";
+
+const Lights = () => {
+  const ref = useRef();
+  const helper = useHelper(ref, THREE.PointLightHelper, 0.5, "red");
+  const { color, distance, decay, intensity } = useControls({
+    color: "#ff0000",
+    distance: 3,
+    decay: 2,
+    intensity: 0.5,
+  });
+
+  // const helper = useHelper(ref, THREE.SpotLightHelper, "red");
+  // const { color, distance, attenuation, angle, anglePower } = useControls({
+  //   color: "#876ae5",
+  //   distance: 6,
+  //   attenuation: 2.2,
+  //   angle: 1,
+  //   anglePower: 1,
+  // });
+
+  return (
+    <>
+      <pointLight position={[1, 1, 1]} ref={ref} color={color} distance={distance} decay={decay} intensity={intensity} />
+      {/* <SpotLight ref={ref} color={color} distance={distance} angle={angle} attenuation={attenuation} anglePower={anglePower} /> */}
+    </>
+  );
+};
 
 function App() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [0, 3, 3] }}>
+        <Lights />
+
+        <OrbitControls />
+
+        <mesh rotation-y={Math.PI / 4}>
+          <boxGeometry />
+          <meshStandardMaterial color="white" roughness={1} metalness={0} />
+        </mesh>
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5}>
+          <planeGeometry args={[5, 5]} />
+          <meshPhysicalMaterial color="white" clearcoat={0.5} reflectivity={0.8} />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
 
