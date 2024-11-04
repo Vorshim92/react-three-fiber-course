755bdcd92035f861808e66ef0b88557e5c073321
Stefano Scalfari <vorshim92@gmail.com>
2024-11-04 15:58:15 +0100

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import { Environment, OrbitControls, OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
+import { Grid, Stats, useHelper } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { City } from "./components/City";
+import { Leva, useControls } from "leva";
+import { useRef } from "react";
+
+import * as THREE from "three";
+
+const Box = () => {
+  const ref = useRef();
+  useHelper(ref, THREE.BoxHelper, "red");
+
+  const { position, color, opacity, transparent } = useControls({
+    position: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    color: "#ff0000",
+    opacity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    transparent: true,
+  });
+
+  return (
+    <mesh ref={ref} position={[position.x, position.y, position.z]}>
+      <boxGeometry />
+      <meshBasicMaterial color={color} transparent={transparent} opacity={opacity} />
+    </mesh>
+  );
+};
+
 function App() {
   return (
-    <Canvas>
-      <PerspectiveCamera position={[3, 3, 3]} near={5} far={8} />
-      <OrthographicCamera makeDefault position={[1, 1, 1]} left={-2 * (window.innerWidth / window.innerHeight)} right={2 * (window.innerWidth / window.innerHeight)} top={2} bottom={-2} near={-5} />
-      <OrbitControls />
-
-      {/* IGNORE FOR NOW */}
-      <City />
-      <Environment preset="city" />
-    </Canvas>
+    <>
+      <Leva hidden={false} />
+      <Canvas camera={{ position: [3, 3, 3] }}>
+        <Stats />
+        {/* <axesHelper args={[2]} />
+      <gridHelper position-y={-0.5} args={[10, 10, "green", "blue"]} /> */}
+        <Box />
+        <Grid sectionSize={3} sectionColor={"purple"} sectionThickness={1} cellSize={1} cellColor={"#6f6f6f"} cellThickness={0.6} infiniteGrid fadeDistance={50} fadeStrength={5} />
+      </Canvas>
+    </>
   );
 }
 
