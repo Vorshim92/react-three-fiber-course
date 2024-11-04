083254232a384f59d6c1b2b31563c87fd5d83de7
Stefano Scalfari <vorshim92@gmail.com>
2024-11-04 16:50:35 +0100

    lights test + multiple LEVA panel debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
 import { OrbitControls, SpotLight, useHelper } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useRef } from "react";
+import { useMemo } from "react";
 
-import { useControls } from "leva";
+import { Leva, useControls } from "leva";
 
 import * as THREE from "three";
 
-const Lights = () => {
+const PointLights = () => {
   const ref = useRef();
-  const helper = useHelper(ref, THREE.PointLightHelper, 0.5, "red");
-  const { color, distance, decay, intensity } = useControls({
-    color: "#ff0000",
-    distance: 3,
-    decay: 2,
-    intensity: 0.5,
-  });
-
-  // const helper = useHelper(ref, THREE.SpotLightHelper, "red");
-  // const { color, distance, attenuation, angle, anglePower } = useControls({
-  //   color: "#876ae5",
-  //   distance: 6,
-  //   attenuation: 2.2,
-  //   angle: 1,
-  //   anglePower: 1,
-  // });
+  useHelper(ref, THREE.PointLightHelper, 0.5, "red");
+  const options = useMemo(
+    () => ({
+      color: "#ff0000",
+      distance: 2.8,
+      decay: 2,
+      intensity: 1,
+      position: [0.7, 0.7, 0.7],
+    }),
+    []
+  );
+  const lightA = useControls("Point Light A", options);
 
   return (
     <>
-      <pointLight position={[1, 1, 1]} ref={ref} color={color} distance={distance} decay={decay} intensity={intensity} />
-      {/* <SpotLight ref={ref} color={color} distance={distance} angle={angle} attenuation={attenuation} anglePower={anglePower} /> */}
+      <pointLight ref={ref} position={lightA.position} color={lightA.color} distance={lightA.distance} decay={lightA.decay} intensity={lightA.intensity} />
+    </>
+  );
+};
+const SpotLights = () => {
+  const ref = useRef();
+  useHelper(ref, THREE.SpotLightHelper, "red");
+  const options = useMemo(
+    () => ({
+      color: "#fff900",
+      distance: 6,
+      attenuation: 2.2,
+      angle: 1.1,
+      anglePower: 1,
+    }),
+    []
+  );
+  const lightB = useControls("Spot Light B", options);
+
+  return (
+    <>
+      <SpotLight ref={ref} color={lightB.color} distance={lightB.distance} angle={lightB.angle} attenuation={lightB.attenuation} anglePower={lightB.anglePower} />
     </>
   );
 };
@@ -37,7 +54,8 @@ function App() {
   return (
     <>
       <Canvas camera={{ position: [0, 3, 3] }}>
-        <Lights />
+        <PointLights />
+        <SpotLights />
 
         <OrbitControls />
 
