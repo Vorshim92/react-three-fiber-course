62329159be3fbed522384e192e427ad3333de9d6
Stefano Scalfari <vorshim92@gmail.com>
2024-11-06 12:55:12 +0100

    controls end

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { Environment, OrbitControls, OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { City } from "./components/City";
+import { Lighthouse } from "./components/Lighthouse";
+import { MoveableItem } from "./components/MoveableItem";
 function App() {
   return (
-    <Canvas>
-      <PerspectiveCamera position={[3, 3, 3]} near={5} far={8} />
-      <OrthographicCamera makeDefault position={[1, 1, 1]} left={-2 * (window.innerWidth / window.innerHeight)} right={2 * (window.innerWidth / window.innerHeight)} top={2} bottom={-2} near={-5} />
-      <OrbitControls />
-
-      {/* IGNORE FOR NOW */}
-      <City />
-      <Environment preset="city" />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [-1.5, 3, 10], fov: 42 }}>
+        {/* <OrbitControls
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minAzimuthAngle={-Math.PI / 2}
+          maxAzimuthAngle={Math.PI / 2}
+          minDistance={3}
+          maxDistance={10}
+        /> */}
+        {/* <PresentationControls
+          global={false} // Spin globally or by dragging the model
+          cursor={true} // Whether to toggle cursor style on drag
+          snap={true} // Snap-back to center (can also be a spring config)
+          speed={1} // Speed factor
+          zoom={1} // Zoom factor when half the polar-max is reached
+          rotation={[0, 0, 0]} // Default rotation
+          polar={[0, Math.PI / 2]} // Vertical limits
+          azimuth={[-Infinity, Infinity]} // Horizontal limits
+          config={{ mass: 2, tension: 250, friction: 16 }} // Spring config
+        > */}
+        <MoveableItem>
+          <Lighthouse position-y={-1} scale={[0.2, 0.2, 0.2]} />
+        </MoveableItem>
+        {/* </PresentationControls> */}
+        <Environment preset="sunset" />
+      </Canvas>
+    </>
   );
 }
 
